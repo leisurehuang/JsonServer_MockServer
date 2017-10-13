0787d53a2443c7f9fc70535c8e19ff9b90476034
Lei Huang <leisure.huang34@gmail.com>
2017-10-13 11:30:39 +0800

    init user

---
+++ db.js
@@ -0,0 +1,25 @@
+let Mock = require('mockjs');
+let Random = Mock.Random;
+
+module.exports = function () {
+    var data = {
+        users: []
+    };
+
+    for (var i = 0; i < 30; i++) {
+        data.users.push({
+            id: i,
+            name: Random.cname,
+            activeStatus: Random.integer(0, 1),
+            age: Random.integer(0, 50),
+            status: Random.integer(0, 1),
+            nickName: Random.first(),
+            email: Random.email("xmail.com"),
+            phone: Random.integer(13000000000, 13999999999),
+            image: Random.image('70x70', Random.color(), ""),
+            address: Random.county(true)
+        })
+    }
+
+    return data
+}
