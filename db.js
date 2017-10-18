618b1df3310faf28a134d67618b758f08f366f23
Lei Huang <leisure.huang34@gmail.com>
2017-10-18 14:44:13 +0800

    add more info to user

--- db.js
+++ db.js
@@ -12,10 +12,11 @@ module.exports = function () {
             name: Random.cname,
             activeStatus: Random.integer(0, 1),
             age: Random.integer(0, 50),
+            gender:Random.integer(0,3),
             status: Random.integer(0, 1),
             nickName: Random.first(),
             email: Random.email("xmail.com"),
-            phone: Random.integer(13000000000, 13999999999),
+            phone: Random.integer(13000000000, 13999999999).toString(),
             image: Random.image('70x70', Random.color(), ""),
             address: Random.county(true)
         })
